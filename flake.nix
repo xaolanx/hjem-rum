b0f05cefda23f14cf39997f0307ab2546af208ba
NotAShelf <raf@notashelf.dev>
2024-12-31 04:52:15 +0300

    flake: provide formatter for `nix fmt`

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "A module collection for hjem.";
-  
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -12,12 +12,17 @@
 
   outputs = {
     self,
-    #nixpkgs,
+    nixpkgs,
     ...
-  }: {
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"];
+  in {
     nixosModules = {
       hjem-rum = import ./modules/nixos.nix;
       default = self.nixosModules.hjem-rum;
     };
+
+    # Provide the default formatter to invoke on 'nix fmt'.
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
   };
 }
