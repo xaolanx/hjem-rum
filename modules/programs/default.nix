b9eb9381c8659291d4de985729270e775b90c735
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-01-18 11:50:55 -0800

    obs-studio: init module

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -2,6 +2,7 @@
   # if you have a better way to do this, feel free to PR
   imports = [
     ./alacritty.nix
+    ./obs-studio.nix
     ./vscode.nix
   ];
 }
---
+++ modules/programs/obs-studio.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkOption mkEnableOption mkPackageOption;
+  inherit (lib.types) listOf package;
+
+  cfg = config.rum.programs.obs-studio;
+in {
+  options.rum.programs.obs-studio = {
+    enable = mkEnableOption "obs-studio";
+
+    package = mkPackageOption pkgs "obs-studio" {};
+
+    plugins = mkOption {
+      type = listOf package;
+      default = [];
+      example = [
+        pkgs.obs-studio-plugins.wlrobs #for screen capture w/wayland
+        pkgs.obs-studio-plugins.obs-vkcapture #vulkan/opengl game capture
+      ];
+
+      description = ''
+        A list of plugins the obs-studio package will be wrapped with.
+        Set of plugins available in nixpkgs under the obs-studio-plugins set.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    packages = [
+      (pkgs.wrapOBS.override { obs-studio = cfg.package; } {
+        plugins = cfg.plugins;
+      })
+    ];
+  };
+}
