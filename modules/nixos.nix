fbbcf18c4a1d417d907dc4b4ab5a25c10b92b8af
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-01-19 12:45:42 -0800

    nixos.nix: extraModules refactor

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -3,9 +3,9 @@
     # Import the hjem-rum module collection as an extraModule passed into `hjem.users.<username>`
     # This allows the definition of rum modules under `hjem.users.<username>.rum`
     hjem.extraModules = [
-      ({...}: {
+      {
         imports = [./programs];
-      })
+      }
     ];
   };
 }
