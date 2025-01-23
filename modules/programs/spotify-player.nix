d6ef63dda22698e82b31b3bf5c883046299c716a
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-01-23 10:21:14 -0800

    spotify-player: fmt
    this will be the last commit I commit and push on the main branch, since it looks like it might be getting some actual contributions from others.

--- modules/programs/spotify-player.nix
+++ modules/programs/spotify-player.nix
@@ -11,115 +11,114 @@
   toml = pkgs.formats.toml {};
 
   cfg = config.rum.programs.spotify-player;
-in
-  {
-    options.rum.programs.spotify-player = {
-      enable = mkEnableOption "spotify-player";
+in {
+  options.rum.programs.spotify-player = {
+    enable = mkEnableOption "spotify-player";
 
-      package = mkPackageOption pkgs "spotify-player" {};
+    package = mkPackageOption pkgs "spotify-player" {};
 
-      settings = mkOption {
-        type = toml.type;
-        default = {};
-        example = {
-          enable_notify = false;
-          device = {
-            name = "nixos"; # bad hostname, don't do this
-            device_type = "computer";
-            volume = 40;
-            normalization = true;
-          };
+    settings = mkOption {
+      type = toml.type;
+      default = {};
+      example = {
+        enable_notify = false;
+        device = {
+          name = "nixos"; # bad hostname, don't do this
+          device_type = "computer";
+          volume = 40;
+          normalization = true;
         };
-
-        description = ''
-          The configuration converted into TOML and written to
-          `${config.directory}/.config/spotify-player/app.toml`.
-          Please reference https://github.com/aome510/spotify-player/blob/master/docs/config.md#general
-          for config options.
-        '';
       };
 
-      themes = mkOption {
-        type = listOf toml.type;
-        default = [];
-        example = [
+      description = ''
+        The configuration converted into TOML and written to
+        `${config.directory}/.config/spotify-player/app.toml`.
+        Please reference https://github.com/aome510/spotify-player/blob/master/docs/config.md#general
+        for config options.
+      '';
+    };
+
+    themes = mkOption {
+      type = listOf toml.type;
+      default = [];
+      example = [
+        {
+          name = "default2";
+          palette = {
+            black = "black";
+            red = "red";
+            green = "green";
+            yellow = "yellow";
+            blue = "blue";
+            magenta = "magenta";
+            cyan = "cyan";
+            white = "white";
+            bright_black = "bright_black";
+            bright_red = "bright_red";
+            bright_green = "bright_green";
+            bright_yellow = "bright_yellow";
+            bright_blue = "bright_blue";
+            bright_magenta = "bright_magenta";
+            bright_cyan = "bright_cyan";
+            bright_white = "bright_white";
+          };
+          component_style = {
+            like = {
+              fg = "Red";
+              modifiers = ["Bold"];
+            };
+            selection = {
+              bg = "Black";
+              fg = "White";
+              modifiers = ["Bold"];
+            };
+            secondary_row = {
+              bg = "#677075";
+            };
+          };
+        }
+      ];
+      description = ''
+        The theme converted into TOML and written to
+        `${config.directory}/.config/spotify-player/themes.toml`.
+        Please reference https://github.com/aome510/spotify-player/blob/master/docs/config.md#themes
+        for config options.
+      '';
+    };
+
+    keymap = mkOption {
+      type = toml.type;
+      default = {};
+      example = {
+        keymaps = [
           {
-            name = "default2";
-            palette = {
-              black = "black";
-              red = "red";
-              green = "green";
-              yellow = "yellow";
-              blue = "blue";
-              magenta = "magenta";
-              cyan = "cyan";
-              white = "white";
-              bright_black = "bright_black";
-              bright_red = "bright_red";
-              bright_green = "bright_green";
-              bright_yellow = "bright_yellow";
-              bright_blue = "bright_blue";
-              bright_magenta = "bright_magenta";
-              bright_cyan = "bright_cyan";
-              bright_white = "bright_white";
-            };
-            component_style = {
-              like = {
-                fg = "Red";
-                modifiers = ["Bold"];
-              };
-              selection = {
-                bg = "Black";
-                fg = "White";
-                modifiers = ["Bold"];
-              };
-              secondary_row = {
-                bg = "#677075";
-              };
-            };
+            command = "NextTrack";
+            key_sequence = "g n";
           }
         ];
-        description = ''
-          The theme converted into TOML and written to
-          `${config.directory}/.config/spotify-player/themes.toml`.
-          Please reference https://github.com/aome510/spotify-player/blob/master/docs/config.md#themes
-          for config options.
-        '';
+        actions = [
+          {
+            action = "GoToArtist";
+            key_sequence = "g A";
+          }
+        ];
       };
-
-      keymap = mkOption {
-        type = toml.type;
-        default = {};
-        example = {
-          keymaps = [
-            {
-              command = "NextTrack";
-              key_sequence = "g n";
-            }
-          ];
-          actions = [
-            {
-              action = "GoToArtist";
-              key_sequence = "g A";
-            }
-          ];
-        };
-        description = ''
-          Sets of keymaps and actions converted into TOML and written to
-          `${config.directory}/.config/spotify-player/keymap.toml`.
-          See example for how to format declarations.
-          Please reference https://github.com/aome510/spotify-player/blob/master/docs/config.md#keymaps
-          for more information.
-        '';
-      };
+      description = ''
+        Sets of keymaps and actions converted into TOML and written to
+        `${config.directory}/.config/spotify-player/keymap.toml`.
+        See example for how to format declarations.
+        Please reference https://github.com/aome510/spotify-player/blob/master/docs/config.md#keymaps
+        for more information.
+      '';
     };
+  };
 
-    config = mkIf cfg.enable {
-      packages = [cfg.package];
-      files = {
-        ".config/spotify-player/app.toml".source = toml.generate "spotify-player/app.toml" cfg.settings;
-        ".config/spotify-player/theme.toml".source = toml.generate "spotify-player/theme.toml" {inherit (cfg) themes;}; # Passes each declared theme under the "themes" attr as needed
-        ".config/spotify-player/keymap.toml".source = toml.generate "spotify-player/keymap.toml" cfg.keymap;
-      };
+  config = mkIf cfg.enable {
+    packages = [cfg.package];
+    files = {
+      ".config/spotify-player/app.toml".source = toml.generate "spotify-player/app.toml" cfg.settings;
+      ".config/spotify-player/theme.toml".source = toml.generate "spotify-player/theme.toml" {inherit (cfg) themes;}; # Passes each declared theme under the "themes" attr as needed
+      ".config/spotify-player/keymap.toml".source = toml.generate "spotify-player/keymap.toml" cfg.keymap;
     };
-  }
+  };
+}
