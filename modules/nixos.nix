ff995377fc25263457d673082d87ca482307736f
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-01-23 16:47:05 -0800

    nixosModules: refactor to match standards

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -1,13 +1,17 @@
-{lib}: {
+{lib}: let
+  inherit (lib.attrsets) filterAttrs attrNames;
+  inherit (lib.trivial) pipe;
+  inherit (builtins) readDir;
+in {
   config = {
     # Import the hjem-rum module collection as an extraModule passed into `hjem.users.<username>`
     # This allows the definition of rum modules under `hjem.users.<username>.rum`
     hjem.extraModules = [
       {
-        imports = lib.pipe ./programs [
-          builtins.readDir
-          (lib.filterAttrs (_: v: v == "regular"))
-          lib.attrNames
+        imports = pipe ./programs [
+          readDir
+          (filterAttrs (_: v: v == "regular"))
+          attrNames
           (map (n: ./programs/${n}))
         ];
       }
