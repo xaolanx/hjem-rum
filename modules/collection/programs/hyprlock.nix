144feef77eda7cc1e62bd9efb7fbdf0e3656b88c
nydragon <git@ccnlc.eu>
2025-04-02 20:16:38 +0200

    hyprlock: init module

---
+++ modules/collection/programs/hyprlock.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (lib.modules) mkIf;
+  inherit (lib.options) mkEnableOption mkPackageOption mkOption;
+  inherit (lib.rum.generators.hypr) toHyprconf;
+  inherit (lib.rum.types) hyprType;
+
+  cfg = config.rum.programs.hyprlock;
+in {
+  options.rum.programs.hyprlock = {
+    enable = mkEnableOption "hyprlock";
+
+    package = mkPackageOption pkgs "hyprlock" {};
+
+    settings = mkOption {
+      type = hyprType;
+      default = {};
+      example = {
+        general = {
+          hide_cursor = true;
+          no_fade_in = false;
+        };
+
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 3;
+            blur_size = 8;
+          }
+        ];
+      };
+      description = ''
+        Is written to `''${config.directory}/hypr/hyprlock.conf`.
+
+        Configuration options can be found on the [Hyprland Wiki](https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock).
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    packages = [cfg.package];
+    files.".config/hypr/hyprlock.conf".text = mkIf (cfg.settings != {}) (toHyprconf {
+      attrs = cfg.settings;
+    });
+  };
+}
