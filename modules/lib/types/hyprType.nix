0e5b1bc643b72a054a1ced5c91f4fa072c7072cb
Anthony Rodriguez <anthony@nezia.dev>
2025-04-04 17:43:11 +0200

    types: update hyprType to match upstream Hyprland module

--- modules/lib/types/hyprType.nix
+++ modules/lib/types/hyprType.nix
@@ -1,8 +1,18 @@
+# copied from https://github.com/hyprwm/Hyprland/blob/ff97d18c4c61ae14f8f3b80178e6b72c8a4b7901/nix/module.nix#L23-L38
 {lib}: let
-  inherit (lib.types) attrsOf listOf oneOf bool float int path str;
-
-  hyprValue = oneOf [str path bool int float hyprList hyprMap];
-  hyprMap = attrsOf hyprValue;
-  hyprList = listOf hyprValue;
+  inherit (lib.types) attrsOf listOf nullOr oneOf bool float int path str;
+  valueType =
+    nullOr (oneOf [
+      bool
+      int
+      float
+      str
+      path
+      (attrsOf valueType)
+      (listOf valueType)
+    ])
+    // {
+      description = "Hyprland configuration value";
+    };
 in
-  hyprMap
+  valueType
