a6dc5f2bdea3710bf273d602823101352958437d
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-04-08 10:45:05 -0700

    nixos: remove osconfig
    osconfig is now provided upstream in hjem, so there is no need to include it in our module specifically

--- modules/nixos.nix
+++ modules/nixos.nix
@@ -1,7 +1,7 @@
 {
   lib,
   rumLib,
-}: {config, ...}: {
+}: {
   # Import the hjem-rum module collection as an extraModule passed into `hjem.users.<username>`
   # This allows the definition of rum modules under `hjem.users.<username>.rum`
   hjem = {
@@ -10,10 +10,7 @@
         imports = lib.filesystem.listFilesRecursive ./collection;
       }
     ];
-    specialArgs = {
-      inherit lib rumLib;
-      osConfig = config;
-    };
+    specialArgs = {inherit lib rumLib;};
   };
 
   warnings = ["The Hjem Rum NixOS Module is soon to be deprecated in favor of a Hjem Module. Please check the updated README for more information."];
