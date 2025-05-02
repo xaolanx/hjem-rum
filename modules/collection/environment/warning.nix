5d4878053e18c54e337e2e532a5db60db95a0109
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-05-01 19:20:25 -0700

    misc/environment: Add a warning for when sessionVariables exist but are not loaded by Hjem Rum

---
+++ modules/collection/environment/warning.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  inherit (lib.options) mkEnableOption;
+  inherit (lib.modules) mkIf;
+  inherit (lib.lists) optionals all;
+
+  # A list checking all modules that load variables. Enabled modules
+  # will evaluate to true, disabled will evaluate to false. Results
+  # in a list of bools, allowing us to
+  variableLoaders = [
+    config.rum.programs.zsh.enable
+    config.rum.programs.fish.enable
+    config.rum.programs.hyprland.enable
+  ];
+
+  cfg = config.rum.environment;
+in {
+  options.rum.environment.hideWarning = mkEnableOption "a warning for env vars not being implemented";
+
+  config = mkIf (!cfg.hideWarning) {
+    # If all modules are disabled, then Hjem Rum is not loading variables
+    warnings = optionals ((all (module: !module) variableLoaders) && (config.environment.sessionVariables != {})) [
+      "environment.sessionVariables exist but are not being loaded by any Hjem Rum modules. Please see the README for more information. If you would like to disable this warning, enable rum.environment.hideWarning."
+    ];
+  };
+}
